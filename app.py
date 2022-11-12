b45f3ebb994a6a43856026dea03345377a74283c
= <=>
2022-11-12 10:47:51 +0000

    Created and configured Sass file for styling HTML

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask, render_template
 
+STATIC_FOLDER = os.path.dirname(__file__) + '/static/'
+
 app = Flask(
     import_name = __name__,
-    template_folder = os.path.dirname(__file__) + '/static/html'
+    template_folder = STATIC_FOLDER + 'html',
+    static_folder = STATIC_FOLDER
 )
 
 
