e82775c099941b4c53c553c85f53a2952db19be8
= <=>
2022-11-12 10:29:44 +0000

    Changed templates folder and created index HTML

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    import_name = __name__,
+    template_folder = os.path.dirname(__file__) + '/static/html'
+)
+
+
 
 @app.route('/')
 def index():
-    return "test"
+    return render_template('index.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
