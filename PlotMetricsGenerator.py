0ac6549d7d5be9861b9e929f7e494adda0d4553a
= <=>
2022-11-18 18:20:47 +0000

    Renamed PlotGenerator class to PlotMetricsGenerator

--- PlotMetricsGenerator.py
+++ PlotMetricsGenerator.py
@@ -1,7 +1,6 @@
-import time
 import random
 
-class PlotGenerator:
+class PlotMetricsGenerator:
     def __init__(self):
         self.plot_id = random.randint(1, 99999)
         self.soil_temp = random.randint(-20, 100)
@@ -13,13 +12,12 @@ class PlotGenerator:
 
     def package_data(self):
         data = {
-                "plot" : self.plot_id,
-                "model" : self.soil_temp,
-                "soil_ph" : self.soil_ph,
-                "co2" : self.co2,
-                "moisture" : self.soil_moisture,
-                "light" : self.light,
-                }
-        return data
+            "plot" : self.plot_id,
+            "model" : self.soil_temp,
+            "soil_ph" : self.soil_ph,
+            "co2" : self.co2,
+            "moisture" : self.soil_moisture,
+            "light" : self.light
+        }
 
-    
+        return data
