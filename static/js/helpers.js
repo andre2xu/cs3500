f39793b2e4b0b134e449f1b14e089943f7260a2c
= <=>
2022-11-19 15:11:14 +0000

    Added zero padding to temporal metrics

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -1,7 +1,26 @@
 export function updateTemporalMetrics(calendar, clock) {
     const DATE = new Date();
 
-    calendar.innerText = `Date: ${DATE.getDate()}/${DATE.getMonth() + 1}/${DATE.getFullYear()}`;
+    let day = `${DATE.getDate()}`;
+    let month = `${DATE.getMonth() + 1}`;
+    let year = `${DATE.getFullYear()}`;
+    let hrs = `${DATE.getUTCHours()}`;
+    let mins = `${DATE.getUTCMinutes()}`;
 
-    clock.innerText = `Time: ${DATE.getUTCHours()}:${DATE.getUTCMinutes()}`;
+    if (day.length === 1) {
+        day = '0' + day;
+    }
+    if (month.length === 1) {
+        month = '0' + month;
+    }
+    if (hrs.length === 1) {
+        hrs = '0' + hrs;
+    }
+    if (mins.length === 1) {
+        mins = '0' + mins;
+    }
+
+    calendar.innerText = `Date: ${day}/${month}/${year}`;
+
+    clock.innerText = `Time: ${hrs}:${mins}`;
 }
