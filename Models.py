71edd8bb5ad1709dcfe91f308b561f4fef479b01
= <=>
2022-11-19 19:58:53 +0000

    Added pH range to forms and CropData model. Also re-doing addCrop API

--- Models.py
+++ Models.py
@@ -5,28 +5,40 @@ db = SQLAlchemy()
 class CropData(db.Model):
     __tablename__ = "cropdata"
 
-    plot_num = db.Column(db.Integer, primary_key = True)
+    plot_num = db.Column(db.Integer, primary_key = True) 
     days_for_growth = db.Column(db.Integer, default = 0)
-    crop_temp = db.Column(db.Float, default = 0)
-    co2_level = db.Column(db.Float, default = 0)
-    seed_temp = db.Column(db.Float, default = 0)
-    light_exp = db.Column(db.Float, default = 0)
-    water_depth = db.Column(db.Float,default = 0)
-    water_interval = db.Column(db.Float,default = 0)
+    seed_min_temp = db.Column(db.Float, default = 0) # celsius
+    seed_max_temp = db.Column(db.Float, default = 0) # celsius
+    crop_min_temp = db.Column(db.Float, default = 0) # celsius
+    crop_max_temp = db.Column(db.Float, default = 0) # celsius
+    min_pH = db.Column(db.Float, default = 0)
+    max_pH = db.Column(db.Float, default = 0)
+    min_co2 = db.Column(db.Integer, default = 0) # ppm
+    max_co2 = db.Column(db.Integer, default = 0) # ppm
+    light_exp = db.Column(db.Float, default = 0) # hrs
+    water_depth = db.Column(db.Float,default = 0) # inches
+    watering_interval = db.Column(db.Float,default = 0) # hrs
 
     def create(self):
         db.session.add(self)
         db.session.commit()
+
         return self
-    def __init__(self, plot_num, days_for_growth,crop_temp,co2_level,seed_temp,light_exp,water_depth,water_interval):
+
+    def __init__(self, plot_num, days_for_growth, seed_min_temp, seed_max_temp, crop_min_temp, crop_max_temp,min_pH, max_pH, min_co2, max_co2, light_exp, water_depth, watering_interval):
         self.plot_num = plot_num
         self.days_for_growth = days_for_growth
-        self.crop_temp = crop_temp
-        self.co2_level = co2_level
-        self.seed_temp = seed_temp
+        self.seed_min_temp = seed_min_temp
+        self.seed_max_temp = seed_max_temp
+        self.crop_min_temp = crop_min_temp
+        self.crop_max_temp = crop_max_temp
+        self.min_pH = min_pH
+        self.max_pH = max_pH
+        self.min_co2 = min_co2
+        self.max_co2 = max_co2
         self.light_exp = light_exp
         self.water_depth = water_depth
-        self.water_interval = water_interval
+        self.watering_interval = watering_interval
 
     def __repr__(self):
         return f"{self.plot_num}"
--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os, mimetypes
-from flask import Flask, request,render_template,redirect,url_for
+from flask import Flask, request, render_template,redirect,url_for
+from Models import db, CropData
 
-from Models import db,CropData
-STATIC_FOLDER = os.path.dirname(__file__) + '/static/'
+
 
 mimetypes.add_type('application/javascript', '.js')
-
+STATIC_FOLDER = os.path.dirname(__file__) + '/static/'
 db_path = os.path.abspath("database.db")
+
+
 
 app = Flask(
     import_name=__name__,
@@ -16,50 +18,40 @@ app = Flask(
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+
+
 db.init_app(app)
 with app.app_context():
     db.create_all()
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route("/api/addCrop", methods=["POST"])
+def addCrop():
+    plotNum = request.form['addPlotNum']
+    harvestCountdown = request.form['addHarvestCountdown']
+    seedMinTemp = request.form['addSeedTemperatureV1']
+    seedMaxTemp = request.form['addSeedTemperatureV2']
+    cropMinTemp = request.form['addCropTemperatureV1']
+    cropMaxTemp = request.form['addCropTemperatureV2']
+    minpH = request.form['addpHV1']
+    maxpH = request.form['addpHV2']
+    minCO2 = request.form['addCO2V1']
+    maxCO2 = request.form['addCO2V2']
+    lightExposureDuration = request.form['addLightDuration']
+    waterDepth = request.form['addWaterDepth']
+    wateringInterval = request.form['addWateringInterval']
 
-@app.route("/api/addCrop/<plotNum>", methods=["GET","POST"])
-def addCrop(plotNum):
-    plot = CropData.query.filter_by(plot_num = plotNum).first()
+    plotData = CropData.query.filter_by(plot_num=plotNum).first()
 
-    if plot:
-        db.session.delete(plot)
-        db.session.commit()
-
-        days_for_growth = request.form["days_for_growth"]
-        seed_temp = request.form["seed_temp"]
-        crop_temp = request.form["crop_temp"]
-        co2_level = request.form["co2_level"]
-        light_exp = request.form["light_exp"]
-        water_depth = request.form["water_depth"]
-        water_interval = request.form["water_interval"]
-
-        plot = CropData(
-            plot_num=plotNum,
-            seed_temp=seed_temp,
-            days_for_growth=days_for_growth,
-            crop_temp=crop_temp,
-            co2_level=co2_level,
-            light_exp=light_exp,
-            water_depth=water_depth,
-            water_interval=water_interval
-        )
-
-        db.session.add(plot)
-        db.session.commit()
-    else:
-        print(f"Plot number {plotNum} does not exist")
+    if (plotData == None):
+        pass # add plot data to database
 
     return redirect(url_for('index'))
-
 
 @app.route('/api/deleteCrop/<plotNum>', methods=['GET'])
 def deleteCrop(plotNum):
